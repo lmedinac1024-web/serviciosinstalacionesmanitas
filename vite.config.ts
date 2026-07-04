4b3fe00f68b36be1a9c83456ffd781545140c860
lmedinac1024-web <lmedinac1024@gmail.com>
2026-07-04 15:57:04 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { nitro } from "nitro/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -7,6 +8,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
+    tailwindcss(),
     tanstackStart(),
     nitro(),
     viteReact(),
